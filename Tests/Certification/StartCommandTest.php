ef71e2377ceb79315d88dd9888b9ff1cf260170a
Martin Aarhof <martin.aarhof@gmail.com>
2014-06-19 10:55:59 +0200

    correct licence description

--- Tests/Certification/StartCommandTest.php
+++ Tests/Certification/StartCommandTest.php
@@ -1,9 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lsv
- * Date: 6/19/14
- * Time: 10:25 AM
+/*
+ * This file is part of the Eko\FeedBundle Symfony bundle.
+ *
+ * (c) Vincent Composieux <vincent.composieux@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Certificationy\Tests;
@@ -14,6 +16,11 @@ use Certificationy\Command\StartCommand;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
 
+/**
+ * StartCommandTest
+ *
+ * @author Vincent Composieux <vincent.composieux@gmail.com>
+ */
 class StartCommandTest extends \PHPUnit_Framework_TestCase
 {
 
